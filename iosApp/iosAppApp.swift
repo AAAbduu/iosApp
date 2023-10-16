daddaae72ff53fa7358a53813eca390d8409d64e
abdu <anawabduali@gmail.com>
2023-10-16 12:50:49 +0200

    Initial Commit

---
+++ iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Abdurrahim Ali on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
